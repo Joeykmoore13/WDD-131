928742f2153c639a9002d784dd33332871ab1f4b
Joeykmoore13 <168593468+Joeykmoore13@users.noreply.github.com>
2026-01-07 15:41:21 -0700

    Fixed some code in JavaScript testing

--- JavaScript testing/variables.js
+++ JavaScript testing/variables.js
@@ -1,24 +1,14 @@
-const pi = 3.1415926535;
+const PI = 3.14;
 let radius = 3;
 
-let area = pi * (radius ** 2);
-console.log(`Area: ${area}`);
+const one = 1;
+const two = '2';
 
-const one = 1
-const two = "2"
-let result = one * two
-// type coerison assumes two was supposed to be an int and converts it automatically
-console.log(result)
-// this concatenates instead of using type coersion
-result = one + two
-console.log(result)
-
-// let i = 1
-// while (i < 10){
-//     console.log(i)
-//     i++;
-// }
-
-for (i = 0; i < 20; i++){
-    console.log(i)
+let course = "CSE131";
+if (true) {
+    let student = "John";
+    console.log(course); 
+    console.log(student);
 }
+console.log(course);
+console.log(student);
